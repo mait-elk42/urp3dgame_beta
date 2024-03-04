1c4bd379ce16bbd740e01782c2fe1d942a45e85c
Mohamed Ait Elkadi <mait-elk@e3r3p16.1337.ma>
2024-03-04 14:19:31 +0100

    finish

---
+++ Scripts/player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 offset;
+    private Camera maincam;
+    [SerializeField]
+    private float speed = 500;
+    void Awake()
+    {
+        maincam = Camera.main;
+    }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        maincam.transform.position = this.transform.position + offset;
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
