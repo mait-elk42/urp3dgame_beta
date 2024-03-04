6797d49054678dfdb955aaf62739e9d0f5f845c1
Mohamed Ait Elkadi <mait-elk@e3r3p16.1337.ma>
2024-03-04 18:54:39 +0100

    done

--- Scripts/player.cs
+++ Scripts/player.cs
@@ -7,21 +7,49 @@ public class player : MonoBehaviour
     [SerializeField]
     private Vector3 offset;
     private Camera maincam;
+    //[SerializeField]
+    //private float speed = 500;
     [SerializeField]
-    private float speed = 500;
+    private GameObject left_foot_target;
+    [SerializeField]
+    private GameObject right_foot_target;
     void Awake()
     {
         maincam = Camera.main;
     }
     void Start()
     {
+        
+    }
 
+    void player_move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            left_foot_target.transform.position += Vector3.forward * Time.deltaTime;
+            //right_foot_target.transform.position += Vector3.forward * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            left_foot_target.transform.position += Vector3.back * Time.deltaTime;
+            //right_foot_target.transform.position += Vector3.back * Time.deltaTime;
+        }
+        //transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        //if (Input.GetKey(KeyCode.S))
+        //    transform.Translate(Vector3.back * speed * Time.deltaTime);
+        //if (Input.GetKey(KeyCode.A))
+        //    transform.Translate(Vector3.left * speed * Time.deltaTime);
+        //if (Input.GetKey(KeyCode.D))
+        //    transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+
+    void camera_follow()
+    {
+        maincam.transform.position = Vector3.Lerp(maincam.transform.position, this.transform.position + offset, 10);
     }
 
     void Update()
     {
-        maincam.transform.position = this.transform.position + offset;
-        if (Input.GetKey(KeyCode.W))
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        player_move();
     }
 }
