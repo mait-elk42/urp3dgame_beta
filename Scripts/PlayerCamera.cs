880415278e7a135f27d54af62aa16baf94d2b8de
Mohamed Ait Elkadi <mait-elk@e3r3p16.1337.ma>
2024-03-15 19:53:48 +0000

    ...

--- Scripts/PlayerCamera.cs
+++ Scripts/PlayerCamera.cs
@@ -2,36 +2,43 @@ using UnityEngine;
 
 public class PlayerCamera
 {
-    private GameObject target;
-    private Vector3 pos;
-    private Camera cam;
-    private float angle;
-    private float distance;
-    private float xsens;
-    private float ysens;
-    private float height;
+    private bool        enabled;
+    private GameObject  player;
+    private Camera      main_camera;
+    private float        camera_sensitivity;
+    private float        xRotation;
+    private Vector3      camera_offset;
+    private float        y_min;
+    private float        y_max;
 
-    public PlayerCamera(GameObject target, Camera cam, float distance, float xsens, float ysens)
+    public PlayerCamera(GameObject player, Camera main_camera, float camera_sensitivity, Vector3 camera_offset, float y_min, float y_max)
     {
-        this.target = target;
-        this.cam = cam;
-        this.distance = distance;
-        this.xsens = xsens;
-        this.ysens = ysens;
-        this.pos = Vector3.zero;
+        this.enabled = false;
+        this.player = player;
+        this.main_camera = main_camera;
+        this.camera_sensitivity = camera_sensitivity;
+        this.camera_offset = camera_offset;
+        this.y_min = y_min;
+        this.y_max = y_max;
     }
 
-    public void Follow(float x, float y)
+    public void enable()
     {
-        angle += x * xsens * Time.deltaTime;
-        if (height + y < 5 && height + y > -5)
-            height += y * ysens * Time.deltaTime;
+        this.enabled = true;
+    }
+    public void disable()
+    {
+        this.enabled = false;
+    }
+    public void Update(float x, float y)
+    {
+        main_camera.transform.position = player.transform.position + camera_offset;
+        if (this.enabled == false)
+            return;
+        xRotation -= y * camera_sensitivity * Time.deltaTime;
+        xRotation = Mathf.Clamp(xRotation, y_min, y_max);
 
-        pos.x = Mathf.Sin(angle) * distance;
-        pos.y = height;
-        pos.z = Mathf.Cos(angle) * distance;
-
-        cam.transform.position = pos + this.target.transform.position;
-        cam.transform.LookAt(this.target.transform.position);
+        main_camera.transform.rotation = Quaternion.Euler(xRotation, player.transform.eulerAngles.y, 0f);
+        player.transform.Rotate(Vector3.up * x * camera_sensitivity * Time.deltaTime);
     }
 }
--- Scripts/player.cs
+++ Scripts/player.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private PlayerCamera player_cam;
-    public float xsens = 10f;
-    public float ysens = 10f;
+
+    private PlayerCamera player_camera;
+    private CharacterController controller;
+    private Vector3 dir;
+    public float speed;
     void Awake()
     {
-        this.player_cam = new PlayerCamera(this.gameObject, Camera.main, 5, xsens, ysens);
+        dir = Vector3.zero;
+        this.player_camera = new PlayerCamera(this.gameObject, Camera.main,  500, new Vector3(0f, 0.5f, 0f), -70, 70);
+        controller = this.GetComponent<CharacterController>();
+    }
+    void Start()
+    {
+        player_camera.enable();
+        
     }
     void Update()
     {
-        player_cam.Follow(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        player_camera.Update(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        if (Input.GetKey(KeyCode.W))
+            dir = transform.forward;
+        controller.Move(dir * speed * Time.deltaTime);
+        dir = Vector3.zero;
     }
 }
